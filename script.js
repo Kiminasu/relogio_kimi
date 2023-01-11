6259563983eb9667972ab3b7b428bb5be0260761
Lucas Q. Oliveira <115563184+Kiminasu@users.noreply.github.com>
2023-01-11 17:09:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ var data = new Date();
 var hora = data.getHours();
 msg.innerHTML = `Agora são ${hora} horas.`;
 if (hora >= 0 && hora < 12){
-    imagem.innerHTML = '<img src="/img/manhã.png">'
+    imagem.innerHTML = '<img src="../img/manhã.png">'
     window.document.body.style.background = '#91aa4c'
 } else if (hora >= 12 && hora < 18){
-    imagem.innerHTML = '<img src="/img/tarde.png">'
+    imagem.innerHTML = '<img src="../img/tarde.png">'
     window.document.body.style.background = '#f3a78d'
 } else{
-    imagem.innerHTML = '<img src="/img/noite.png">'
+    imagem.innerHTML = '<img src="../img/noite.png">'
     window.document.body.style.background = '#0067c8'
 }
 }
